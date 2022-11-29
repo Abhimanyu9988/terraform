949310114fc788c1cf08695c2e823d1af6046e42
Abhi <abhimanyubajaj98@gmail.com>
2022-11-28 18:37:58 -0600

    checking

--- main.tf
+++ main.tf
@@ -21,6 +21,6 @@ resource "aws_instance" "AbhiBajLinux" {
   subnet_id = var.subnet_id
   instance_type = var.instance_type
   associate_public_ip_address = true
-  vpc_security_group_ids = ["sg-01da51cf8ccfc2ff6"] #This is in region us-east-1, If you are changing the SG, Please change the region on line 13 and add the new SG
+  vpc_security_group_ids = ["sg-01da51cf8ccfc2ff6"] #This is in region us-east-1, If you are changing the SG, Please change the region on line 13 and add the new security group
   tags = var.tags
 }
